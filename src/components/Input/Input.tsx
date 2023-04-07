be761d1a3a6a1d31633307133c6603492338da70
Jerrext <113127858+Jerrext@users.noreply.github.com>
2023-04-07 15:32:59 +0300

    Add colors and Input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,54 @@
+import React, { ChangeEvent, FC, KeyboardEvent } from "react";
+import classNames from "classnames";
+import styles from "./Input.module.scss";
+
+type InputProps = {
+  value: string;
+  title?: string;
+  placeholder: string;
+  onKeyDown?: (event: KeyboardEvent<HTMLInputElement>) => void;
+  inputType: string;
+  disabled?: boolean;
+  errText?: string;
+  className?: string;
+  onChange: (value: string) => void;
+  onBlur?: () => void;
+};
+
+const Input: FC<InputProps> = ({
+  value,
+  title,
+  placeholder,
+  inputType,
+  disabled,
+  errText,
+  className,
+  onChange,
+  onBlur,
+  onKeyDown,
+}) => {
+  const onChangeText = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value);
+  };
+  return (
+    <div className={styles.inputWrapper}>
+      <p className={styles.title}>{title}</p>
+      <input
+        value={value}
+        className={classNames(styles.input, className, {
+          [styles.disabledInp]: disabled,
+          [styles.errorInput]: errText,
+        })}
+        type={inputType}
+        onKeyDown={onKeyDown}
+        placeholder={placeholder}
+        onBlur={onBlur}
+        disabled={disabled}
+        onChange={onChangeText}
+      />
+      {errText && <p className={styles.errorText}>{errText}</p>}
+    </div>
+  );
+};
+
+export default Input;
--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "src/pages/SignIn/SignIn.module.scss";
+import Input from "src/components/Input/";
 
 const SignIn = () => {
-  return <div></div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className={styles.signInWrapper}>
+      <Input
+        value={email}
+        placeholder="Your email"
+        title="Email"
+        inputType="email"
+        onChange={setEmail}
+      />
+      <Input
+        value={password}
+        placeholder="Your password"
+        title="Password"
+        inputType="password"
+        onChange={setPassword}
+      />
+    </div>
+  );
 };
 
 export default SignIn;
